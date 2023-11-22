fe51e9dbfc2d36daca0597081f00263a4f421a65
ejsmith13 <ejsmithwork@gmail.com>
2023-11-21 22:59:53 -0500

    3.1

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,8 @@
+const EQUATION: u32 = 3 * 60;
 fn main() {
     let mut x = 5;
     println!("The value of x is {x}");
     x = 6;
     println!("The value of x is {x}");
-
+    println!("{EQUATION}");
 }
