de810c12f77c1719f2079cbf41c6779796f24ac8
ejsmith13 <ejsmithwork@gmail.com>
2023-11-22 22:56:25 -0500

    equation types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,17 @@
-const EQUATION: u32 = 3 * 60;
 fn main() {
-    let mut x = 5;
-    println!("The value of x is {x}");
-    x = 6;
-    println!("The value of x is {x}");
-    println!("{EQUATION}");
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
 }
