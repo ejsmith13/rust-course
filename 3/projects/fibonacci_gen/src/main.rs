d476782352a6ff291ce3d0534501bf415a163017
ejsmith13 <ejsmithwork@gmail.com>
2023-11-27 14:02:51 -0500

    fibonacci generator project

--- 3/projects/fibonacci_gen/src/main.rs
+++ 3/projects/fibonacci_gen/src/main.rs
@@ -1,3 +1,42 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut n = String::new();
+
+    println!("Enter a number: ");
+
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Please enter a number");
+
+    let n: u32 = n.trim().parse().expect("NAN");
+
+    if n == 1 {
+        println! {"Answer is 1"}
+    } else {
+        fibonacci(n)
+    }
+}
+
+fn fibonacci(n: u32) {
+    // let mut count = 2;
+    let mut a = 0;
+    let mut b = 1;
+    let mut c: u32;
+
+    // while count < (n ) {
+    //     c = a + b;
+    //     a = b;
+    //     b = c;
+
+    //     count += 1
+    // }
+
+    for _ in 2..n{
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    println!("{n}th Fibonacci sequence number is {b}")
 }
