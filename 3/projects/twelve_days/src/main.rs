79e0aa9285b88279a85b887623657745daf5caf7
ejsmith13 <ejsmithwork@gmail.com>
2023-11-27 14:53:59 -0500

    twelve days project

--- 3/projects/twelve_days/src/main.rs
+++ 3/projects/twelve_days/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    let gifts = [
+        "A partridge in a pear tree",
+        "Two turtle doves",
+        "Three French Hens",
+        "Four calling birds",
+        "Five gold rings",
+        "Six geese a laying",
+        "Seven swans a swimming",
+        "Eight maids a milking",
+        "Nine ladies dancing",
+        "Ten lords a leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    for day in 1..=12 {
+        // let mut current= 1;
+        let suffix = if day == 1 {
+            "st"
+        } else if day == 2 {
+            "nd"
+        } else if day == 3 {
+            "rd"
+        } else {
+            "th"
+        };
+        println!("On the {day}{suffix} day of Christmas, My true love gave to me: ");
+        for x in (0..day).rev() {
+            
+            println!("{}", gifts[x]);
+            if x == 1 {
+                println! {"and"}
+            }
+        }
+
+        println!("------------------")
+    }
 }
